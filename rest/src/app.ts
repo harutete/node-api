188fde3ceafffc477cb5b2b7866edc44b9b6c4f1
harutete <ai.takeuchi.10@gmail.com>
2021-08-11 00:57:37 +0900

    ポート番号の変更

--- rest/src/app.ts
+++ rest/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import group from './routes/group'
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)

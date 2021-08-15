303e7b79df89821c9257a36b7084849d43c4085c
harutete <ai.takeuchi.10@gmail.com>
2021-08-16 01:47:54 +0900

    スキーマの修正

--- graphql/src/app.ts
+++ graphql/src/app.ts
@@ -3,20 +3,32 @@ import cors from 'cors'
 
 import { graphqlHTTP } from 'express-graphql'
 import { buildSchema } from 'graphql'
-import { GROUP_LIST } from '../../data/group'
+import { MEMBERS } from '../../data/members'
+import { LINK_SKILLS } from '../../data/linkSkills'
 
 const port = 4000
 const schema = buildSchema(`
-  type Group {
-    group_name: String
+  type Member {
+    id: Int
+    name: String
+  }
+  type LinkSkill {
+    category: String
+    category_name: String
+    effect: Int
+    skill_name: String
+    is_act2: Boolean
+    members: [Member]
   }
   type Query {
-    group: [Group]
+    members: [Member]
+    linkSkills: [LinkSkill]
   }
 `)
 
 const rootValue = {
-  group: () => GROUP_LIST
+  members: () => MEMBERS,
+  linkSkills: () => LINK_SKILLS
 }
 
 const app = express()

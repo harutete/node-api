0fe727e13c3fbd516eb942f5aa0531707db5a398
harutete <ai.takeuchi.10@gmail.com>
2021-08-28 00:01:45 +0900

    schemaを別ファイルに切り出す

--- graphql/src/app.ts
+++ graphql/src/app.ts
@@ -2,29 +2,11 @@ import express from 'express'
 import cors from 'cors'
 
 import { graphqlHTTP } from 'express-graphql'
-import { buildSchema } from 'graphql'
+import { schema } from './schema'
 import { MEMBERS } from '../../data/members'
 import { LINK_SKILLS } from '../../data/linkSkills'
 
 const port = 4000
-const schema = buildSchema(`
-  type Member {
-    id: Int
-    name: String
-  }
-  type LinkSkill {
-    category: String
-    category_name: String
-    effect: Int
-    skill_name: String
-    is_act2: Boolean
-    members: [Member]
-  }
-  type Query {
-    members: [Member]
-    linkSkills: [LinkSkill]
-  }
-`)
 
 const rootValue = {
   members: () => MEMBERS,
---
+++ graphql/src/schema/index.ts
@@ -0,0 +1,20 @@
+import { buildSchema } from 'graphql'
+
+export const schema = buildSchema(`
+  type Member {
+    id: Int
+    name: String
+  }
+  type LinkSkill {
+    category: String
+    category_name: String
+    effect: Int
+    skill_name: String
+    is_act2: Boolean
+    members: [Member]
+  }
+  type Query {
+    members: [Member]
+    linkSkills(member: String, skill: String): [LinkSkill]
+  }
+`)
